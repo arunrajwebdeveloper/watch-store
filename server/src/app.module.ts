9fea60d9339936be285d5e422c10f8aeac854889
arunraj.r <arunraj.r@nergyindia.com>
2025-04-08 10:58:22 +0530

    basic structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule],
+  imports: [AuthModule, UsersModule, ProductsModule, CartModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
