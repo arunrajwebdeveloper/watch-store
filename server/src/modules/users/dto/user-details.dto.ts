2e9a261ce861b76a68206c8f1c279e3995918d24
arunraj.r <arunraj.r@nergyindia.com>
2025-04-11 14:31:50 +0530

    dto-update

--- server/src/modules/users/dto/user-details.dto.ts
+++ server/src/modules/users/dto/user-details.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsMongoId } from 'class-validator';
 import { AddressDto } from '../../common/dto/address.dto';
 
 export class UserDetailsDto extends AddressDto {
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsOptional()
+  @IsString()
   avatar?: string;
 }
