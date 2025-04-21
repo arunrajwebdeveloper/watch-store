f45d2214c91a48f3620ed9776288736fc44f0ce3
arunraj.r <arunraj.r@nergyindia.com>
2025-04-21 16:07:41 +0530

    scrolltop

--- client/src/app/clientProvider.tsx
+++ client/src/app/clientProvider.tsx
@@ -3,11 +3,17 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "@/store";
 import { refreshUser } from "@/store/slices/authSlice";
+import { usePathname } from "next/navigation";
 import { getCart } from "@/store/slices/cartSlice";
 import GlobalSpinner from "@/components/common/GlobalSpinner";
 
 const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useAppDispatch();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   useEffect(() => {
     dispatch(refreshUser());
