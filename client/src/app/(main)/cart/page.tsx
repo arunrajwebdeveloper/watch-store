8da84b83d87a4d05817b711e4f7156a4acb581f2
arunraj.r <arunraj.r@nergyindia.com>
2025-04-29 20:21:36 +0530

    client

--- client/src/app/(main)/cart/page.tsx
+++ client/src/app/(main)/cart/page.tsx
@@ -1,21 +1,17 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import { useAppDispatch, useAppSelector } from "@/store";
 import {
   clearCart,
   removeCartItem,
   updateCartItem,
-  applyCoupon,
-  clearCouponError,
-  removeAppliedCoupon,
 } from "@/store/slices/cartSlice";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { currencyFormat } from "@/utils/currencyFormat";
 
 function CartPage() {
-  const [promocode, setPromocode] = useState("");
   const isLoading = useAppSelector((state) => state.cart.isLoading);
   const cartState = useAppSelector((state) => state.cart);
 
@@ -37,36 +33,6 @@ function CartPage() {
     }
   };
 
-  const createCartBreakdown = (cart: any) => {
-    return {
-      productTotal: { label: "Subtotal", value: cart?.cartTotalAmount },
-      discount: {
-        label: cartState?.appliedCoupon?.code,
-        value: cart?.cartDiscountAmount,
-      },
-      gstAmount: {
-        label: `GST (${cart?.gstPercentage * 100}%)`,
-        value: cart?.gstAmount,
-      },
-      shippingFee: { label: "Shipping Fee", value: cart?.shippingFee },
-      grandTotal: { label: "Total Payable", value: cart?.cartFinalTotalAmount },
-    };
-  };
-
-  const priceBreakdownList = createCartBreakdown(cartState);
-
-  const handleApplyCoupon = async (promocode: string) => {
-    if (promocode) {
-      await dispatch(applyCoupon(promocode.trim())).unwrap();
-      setPromocode("");
-    }
-  };
-
-  const handleCouponChange = (e: any) => {
-    setPromocode(e.target.value);
-    dispatch(clearCouponError());
-  };
-
   return (
     <div className="container">
       <div className="product-listing-page">
@@ -85,10 +51,11 @@ function CartPage() {
                     <th>Price</th>
                     <th>Total</th>
                   </tr>
+
                   {cartState?.cartItems?.length > 0 ? (
                     cartState?.cartItems?.map(({ product, quantity }, idx) => {
                       return (
-                        <tr key={`product-main-list-${product._id}`}>
+                        <tr key={`product-cart-list-${product._id}`}>
                           <td>{++idx}</td>
                           <td>
                             <div className="item-basic-infos">
@@ -212,146 +179,33 @@ function CartPage() {
                       </td>
                     </tr>
                   )}
+
+                  {cartState?.cartItems?.length > 0 && (
+                    <tr className="cart-total-footer">
+                      <td colSpan={4}>Subtotal</td>
+                      <td>
+                        {currencyFormat(
+                          parseInt(cartState.cartTotalAmount.toString())
+                        )}
+                      </td>
+                    </tr>
+                  )}
                 </tbody>
               </table>
             </div>
-          </div>
-          <div className="cart-layout-sidebar">
-            <div className="cart-sidebar-wrap">
-              {cartState?.cartItems?.length > 0 && (
-                <div className="sidebar-box">
-                  <div className="box-header">
-                    <h4>Summary</h4>
-                  </div>
-                  <div className="sidebar-box-content">
-                    <div className="promo-code">
-                      <h4>Do you have a promo code?</h4>
-                      <div>
-                        <div className="promo-code-field">
-                          <input
-                            className="input-element"
-                            placeholder="Promo code"
-                            value={promocode}
-                            onChange={(e) => handleCouponChange(e)}
-                            disabled={cartState.isApplyingCoupon}
-                          />
-                          <button
-                            disabled={cartState.isApplyingCoupon || !promocode}
-                            onClick={() => handleApplyCoupon(promocode)}
-                            className="btn secondary"
-                          >
-                            Apply
-                          </button>
-                        </div>
-                        {cartState?.couponError && (
-                          <div className="invalid-message">
-                            {cartState?.couponError?.message}
-                          </div>
-                        )}
-                        {cartState?.appliedCoupon && (
-                          <div className="applied-promo-code">
-                            <div className="promocode-display">
-                              <span>{cartState?.appliedCoupon?.code}</span>
-                              <button
-                                onClick={() => dispatch(removeAppliedCoupon())}
-                              >
-                                <svg
-                                  width="22px"
-                                  height="22px"
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                >
-                                  <g>
-                                    <path
-                                      d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16"
-                                      stroke="#000000"
-                                      strokeWidth="2"
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                    />
-                                  </g>
-                                </svg>
-                              </button>
-                            </div>
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                    <table className="summary-table">
-                      <tbody>
-                        {Object.entries(priceBreakdownList).map(
-                          ([key, { value, label }]) => {
-                            if (key === "discount") {
-                              if (value !== 0) {
-                                return (
-                                  <tr key={key} style={{ color: "green" }}>
-                                    <td>
-                                      <div
-                                        style={{
-                                          display: "flex",
-                                          alignItems: "center",
-                                          gap: "5px",
-                                        }}
-                                      >
-                                        <svg
-                                          width="18px"
-                                          height="18px"
-                                          viewBox="0 0 24 24"
-                                          fill="none"
-                                        >
-                                          <path
-                                            d="M7.0498 7.0498H7.0598M10.5118 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V10.5118C3 11.2455 3 11.6124 3.08289 11.9577C3.15638 12.2638 3.27759 12.5564 3.44208 12.8249C3.6276 13.1276 3.88703 13.387 4.40589 13.9059L9.10589 18.6059C10.2939 19.7939 10.888 20.388 11.5729 20.6105C12.1755 20.8063 12.8245 20.8063 13.4271 20.6105C14.112 20.388 14.7061 19.7939 15.8941 18.6059L18.6059 15.8941C19.7939 14.7061 20.388 14.112 20.6105 13.4271C20.8063 12.8245 20.8063 12.1755 20.6105 11.5729C20.388 10.888 19.7939 10.2939 18.6059 9.10589L13.9059 4.40589C13.387 3.88703 13.1276 3.6276 12.8249 3.44208C12.5564 3.27759 12.2638 3.15638 11.9577 3.08289C11.6124 3 11.2455 3 10.5118 3ZM7.5498 7.0498C7.5498 7.32595 7.32595 7.5498 7.0498 7.5498C6.77366 7.5498 6.5498 7.32595 6.5498 7.0498C6.5498 6.77366 6.77366 6.5498 7.0498 6.5498C7.32595 6.5498 7.5498 6.77366 7.5498 7.0498Z"
-                                            stroke="green"
-                                            strokeWidth="2"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                          />
-                                        </svg>
-                                        {label}
-                                      </div>
-                                    </td>
-                                    <td>{`- ${currencyFormat(
-                                      parseInt(value.toString())
-                                    )}`}</td>
-                                  </tr>
-                                );
-                              }
-                              return;
-                            }
-                            if (key === "grandTotal") {
-                              return (
-                                <tr key={key} className="grand-total">
-                                  <td>
-                                    <h3>{label}</h3>
-                                  </td>
-                                  <td>
-                                    <h3>
-                                      {currencyFormat(
-                                        parseInt(value.toString())
-                                      )}
-                                    </h3>
-                                  </td>
-                                </tr>
-                              );
-                            }
-                            return (
-                              <tr key={key}>
-                                <td>{label}</td>
-                                <td>
-                                  {currencyFormat(parseInt(value.toString()))}
-                                </td>
-                              </tr>
-                            );
-                          }
-                        )}
-                      </tbody>
-                    </table>
-                    <div className="checkout-final-btn">
-                      <button className="btn primary">Checkout</button>
-                    </div>
-                  </div>
-                </div>
-              )}
+            <div
+              style={{
+                textAlign: "right",
+                marginTop: "30px",
+                marginBottom: "30px",
+              }}
+            >
+              <button
+                onClick={() => router.push("/checkout")}
+                className="btn secondary"
+              >
+                Proceed to Checkout
+              </button>
             </div>
           </div>
         </div>
