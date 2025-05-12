ec61c87bb5005d26a7a7b29663e763b9cae5084e
arunraj.r <arunraj.r@nergyindia.com>
2025-05-12 14:48:16 +0530

    link

--- admin/src/modules/dashboard/view/Statistics.tsx
+++ admin/src/modules/dashboard/view/Statistics.tsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { useDashboard } from "../hook";
 
 export const Statistics = () => {
   const { fetchStatistics } = useDashboard({ load: true });
   const { data, isError } = fetchStatistics;
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+  };
 
   if (isError) return <span>An error occured.</span>;
 
@@ -13,19 +19,22 @@ export const Statistics = () => {
       </div>
       <div className="page-content">
         <div className="stat-boxes">
-          <div className="stat-card">
+          <div
+            className="stat-card"
+            onClick={() => handleNavigate("/products")}
+          >
             <div>
               <h3>Products</h3>
               <p>{data?.productCount}</p>
             </div>
           </div>
-          <div className="stat-card">
+          <div className="stat-card" onClick={() => handleNavigate("/users")}>
             <div>
               <h3>Users</h3>
               <p>{data?.userCount}</p>
             </div>
           </div>
-          <div className="stat-card">
+          <div className="stat-card" onClick={() => handleNavigate("/orders")}>
             <div>
               <h3>Orders</h3>
               <p>{data?.orderCount}</p>
