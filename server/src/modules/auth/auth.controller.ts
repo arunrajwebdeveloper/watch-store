6ef873724cbea438b7461efa201c7afee2d3e0cc
arunraj.r <arunraj.r@nergyindia.com>
2025-05-16 22:13:25 +0530

    fix

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -64,7 +64,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      path: '/api/auth/refresh',
+      path: '/',
     });
     return { message: 'Logged out' };
   }
