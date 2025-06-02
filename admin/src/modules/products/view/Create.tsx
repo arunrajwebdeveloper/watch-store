47da5bf6605e81e4272dbfd899449e83eea356c1
arunraj.r <arunraj.r@nergyindia.com>
2025-06-02 15:58:02 +0530

    fix

--- admin/src/modules/products/view/Create.tsx
+++ admin/src/modules/products/view/Create.tsx
@@ -372,32 +372,34 @@ export const Create = () => {
               </div>
             ))}
 
-            <div
-              className="d-flex image-add-more"
-              onClick={() => append({ url: "" })}
-            >
-              <svg width="80px" height="80px" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M9 12H15"
-                  stroke="#9599b5"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M12 9L12 15"
-                  stroke="#9599b5"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 19.4115 19.4115 21 12 21C4.5885 21 3 19.4115 3 12Z"
-                  stroke="#9599b5"
-                  strokeWidth="1.5"
-                />
-              </svg>
-            </div>
+            {fields.length < 6 && (
+              <div
+                className="d-flex image-add-more"
+                onClick={() => append({ url: "" })}
+              >
+                <svg width="80px" height="80px" viewBox="0 0 24 24" fill="none">
+                  <path
+                    d="M9 12H15"
+                    stroke="#9599b5"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M12 9L12 15"
+                    stroke="#9599b5"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 19.4115 19.4115 21 12 21C4.5885 21 3 19.4115 3 12Z"
+                    stroke="#9599b5"
+                    strokeWidth="1.5"
+                  />
+                </svg>
+              </div>
+            )}
           </div>
         </div>
         <button
